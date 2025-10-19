dbf292c50cddfb29573ba56f4850b90defa5dd21
gibrannn7 <fatihmaulana565@gmail.com>
2025-10-20 06:00:38 +0700

    frontend fix

--- myuniba-frontend/postcss.config.js
+++ myuniba-frontend/postcss.config.js
@@ -1,6 +1,11 @@
+import postcssNesting from 'postcss-nesting';
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    postcssNesting(),
+    tailwindcss(),
+    autoprefixer(),
+  ],
+};
